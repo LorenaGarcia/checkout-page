2ef068595d7c0fb2b7523776f87904fd2374fed7
Lorraine <Lorraine@Isaacs-MacBook-Pro.local>
2020-10-21 16:53:38 -0500

    Updates

---
+++ precache-manifest.4737a0382fc4df032d58f2df1a444e30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d551858f60b8e3070eea6ef91d32e77b",
+    "url": "/checkout-page/index.html"
+  },
+  {
+    "revision": "d25ab535d9e4b0f74a67",
+    "url": "/checkout-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6452166d69cf0161cc2f",
+    "url": "/checkout-page/static/js/2.8042db92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/checkout-page/static/js/2.8042db92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25ab535d9e4b0f74a67",
+    "url": "/checkout-page/static/js/main.b3a40311.chunk.js"
+  },
+  {
+    "revision": "fbd13ab2f1490511efcb",
+    "url": "/checkout-page/static/js/runtime-main.b90ed273.js"
+  },
+  {
+    "revision": "4e7df698deee511bfd14f1787f14f376",
+    "url": "/checkout-page/static/media/photo1.4e7df698.png"
+  },
+  {
+    "revision": "44e5172828f6acf7427e432ddc424ec1",
+    "url": "/checkout-page/static/media/photo2.44e51728.png"
+  }
+]);
